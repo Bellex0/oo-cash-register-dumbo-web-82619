0ce119828bbc90c770afb6652d4e94dfd21f28f2
Belle P <bellepx3@gmail.com>
2019-09-01 23:45:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
       @price = price
-      @total += (quantity * @price)
+      @total += (quantity * @price.to_f)
       counter = 0
       while counter < quantity
       @items << title
